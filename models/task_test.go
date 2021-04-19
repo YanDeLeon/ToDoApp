5d354954ac9805acd933f42acc8d40fc35b580c2
Julian De Leon <juliandeleon@Julians-MacBook-Pro.local>
2021-04-19 12:56:22 -0500

    adding final tests

--- models/task_test.go
+++ models/task_test.go
@@ -1,5 +1,16 @@
 package models
 
+import "github.com/gofrs/uuid"
+
 func (ms *ModelSuite) Test_Task() {
-	ms.Fail("This test needs to be implemented!")
+	id, err := uuid.FromString("468d02bb-98ac-4496-af51-62c3c1f55530")
+	ms.NoError(err)
+	newTask := Task{
+		ID:          id,
+		Name:        "Make tests",
+		Description: "Write all tests for actions",
+	}
+
+	ms.NoError(ms.DB.Create(&newTask))
+
 }
