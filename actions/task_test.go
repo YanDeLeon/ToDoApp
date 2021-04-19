d542811bb9df705e345ee4780e5aadcb9ecab1b1
Julian De Leon <juliandeleon@Julians-MacBook-Pro.local>
2021-04-19 12:47:21 -0500

    adding more tests

--- actions/task_test.go
+++ actions/task_test.go
@@ -65,3 +65,77 @@ func (as *ActionSuite) Test_Show_Task() {
 	as.Contains(res.Body.String(), "Make tests")
 	as.Contains(res.Body.String(), "tests for actions")
 }
+
+func (as *ActionSuite) Test_Edit_Task() {
+	id, err := uuid.FromString("468d02bb-98ac-4496-af51-62c3c1f55530")
+	as.NoError(err)
+	newTask := models.Task{
+		ID:          id,
+		Name:        "Make tests",
+		Description: "Write all tests for actions",
+	}
+
+	as.NoError(as.DB.Create(&newTask))
+
+	res := as.HTML("/").Get()
+	as.Equal(http.StatusOK, res.Code)
+
+	res = as.HTML("/task/468d02bb-98ac-4496-af51-62c3c1f55530/edit").Get()
+	as.Equal(http.StatusOK, res.Code)
+	as.Contains(res.Body.String(), "Edit Task:")
+
+}
+
+func (as *ActionSuite) Test_Update_Task() {
+	id, err := uuid.FromString("468d02bb-98ac-4496-af51-62c3c1f55530")
+	as.NoError(err)
+	newTask := models.Task{
+		ID:          id,
+		Name:        "Make tests",
+		Description: "Write all tests for actions",
+	}
+
+	as.NoError(as.DB.Create(&newTask))
+
+	res := as.HTML("/").Get()
+	as.Equal(http.StatusOK, res.Code)
+
+	res = as.HTML("/task/468d02bb-98ac-4496-af51-62c3c1f55530/edit").Get()
+	as.Equal(http.StatusOK, res.Code)
+	values := url.Values{
+		"Name":        []string{"New Name"},
+		"Description": []string{"New description"},
+	}
+	as.TableChange("tasks", 0, func() {
+		res = as.HTML("/task/468d02bb-98ac-4496-af51-62c3c1f55530/update").Put(values)
+		as.Equal(http.StatusSeeOther, res.Code)
+	})
+
+	res = as.HTML("/").Get()
+	as.Contains(res.Body.String(), "New Name")
+	as.Contains(res.Body.String(), "New description")
+
+}
+
+func (as *ActionSuite) Test_Delete_Task() {
+	id, err := uuid.FromString("468d02bb-98ac-4496-af51-62c3c1f55530")
+	as.NoError(err)
+	newTask := models.Task{
+		ID:          id,
+		Name:        "Make tests",
+		Description: "Write all tests for actions",
+	}
+
+	as.NoError(as.DB.Create(&newTask))
+
+	as.TableChange("tasks", -1, func() {
+		res := as.HTML("/task/468d02bb-98ac-4496-af51-62c3c1f55530/delete").Delete()
+		as.Equal(http.StatusSeeOther, res.Code)
+		as.Equal(res.Location(), "/")
+	})
+
+	res := as.HTML("/").Get()
+	as.Equal(http.StatusOK, res.Code)
+	as.NotContains(res.Body.String(), "Make tests")
+	as.NotContains(res.Body.String(), "Write all tests for actions")
+}
